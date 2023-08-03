0b9d550b0e2d6b7566d583df9028e1faceee12ba
ArdetSorin <ardet.d.sorin@gmail.com>
2023-08-03 18:49:16 +0300

    Commit from main branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         //comment this
-        //cooment from main branch
+        //comment from main branch
         //another commit on main branch
 
     }
