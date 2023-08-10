1475b6518e8b440e47969502a3dac67f54057a6f
ArdetSorin <ardet.d.sorin@gmail.com>
2023-08-10 18:59:48 +0300

    Session 4

---
+++ src/session4/Challenges/Challenge4.java
@@ -0,0 +1,18 @@
+package session4.Challenges;
+
+public class Challenge4 {
+
+/* Write a Java program where you declare two integer variables heightOne and heightTwo
+with any value. Use a ternary operator to determine the greater height and assign it to
+a variable maximumHeight. Print the result.
+ */
+
+    public static void main(String[] args) {
+        int heightOne = 178;
+        int heightTwo = 173;
+
+        int maximumHeight = (heightOne > heightTwo) ? heightOne : heightTwo;
+
+        System.out.println("The maximum height is: " + maximumHeight);
+    }
+}
